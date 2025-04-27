3952ac3a1dc1ee8aa0c189402b2b2931e5f05528
dulalaavas <144573527+dulalaavas@users.noreply.github.com>
2025-04-27 12:36:26 +0545

    Q3 modified.

--- Q3.java
+++ Q3.java
@@ -1,7 +1,11 @@
 public class Q3 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= i; j++) {
+        for (int i = 1; 
+        i <= 5; 
+        i++) {
+            for (int j = 1; 
+            j <= i; 
+            j++) {
                 System.out.print(j);
             }
             System.out.println();
