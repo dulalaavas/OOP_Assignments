c7609ef096b240309efd74a10e52762a9dc1954e
dulalaavas <144573527+dulalaavas@users.noreply.github.com>
2025-04-27 12:24:44 +0545

    Surprise Test Apr 27

---
+++ Q1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String word = sc.nextLine();
+
+        String reversed = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
+        }
+
+        System.out.println("Reversed word: " + reversed);
+    }
+}
